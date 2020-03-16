e0e50216d061c243373e49445211dc87eeea0d96
Ema Jemec <ema.jemec@student.fmf.uni-lj.si>
2020-03-16 10:16:40 +0100

    vaje 4

---
+++ vaje_16_3/IzpisTabele/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IzpisTabele
+{
+    class Program
+    {
+        public static void IzpisTabele<Karkoli>(Karkoli[] tab)
+        {
+            foreach (Karkoli el in tab)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void IzpisiVecji<T>(T pod1, T pod2) where T : IComparable<T>
+        {
+            T vecji;
+            if (pod1.CompareTo(pod2) > 0)
+            {
+                vecji = pod1;
+            }
+            else
+            {
+                vecji = pod2;
+            }
+            Console.WriteLine("Večji podatek je: " + vecji);
+        }
+        static void Main(string[] args)
+        {
+            int[] tabStevil = new int[] {23, 5, 6, 7};
+            string[] tabNizov = new string[] { "bla", "ble", "blo", "blu" };
+            IzpisTabele(tabStevil);
+            IzpisTabele(tabNizov);
+            IzpisiVecji(12, 3);
+        }
+    }
+}
