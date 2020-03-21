a7fce008ed52618adf5a91bc7d54c9a84faf4b2e
Ema Jemec <ema.jemec@student.fmf.uni-lj.si>
2020-03-21 20:00:04 +0100

    vaje 4

---
+++ vaje_16_3/Bisekcija/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Bisekcija
+{
+    class Program
+    {
+        
+        //Za kakrsno koli urejeno tabelo z bisekcijo poisce podatek in vrne True, če ga tabela vsebuje
+        public static bool Bisekcija<T>(T[] tab, T podatek) where T : IComparable<T>
+        {
+            int pol = tab.Length / 2;
+            if (tab.Length == 0)
+            {
+                return false;
+            }
+            if (podatek.CompareTo(tab[pol]) == 0)
+            {
+                return true;
+            }
+            else if (podatek.CompareTo(tab[pol]) > 0)
+            {
+                T[] drugepol = tab.Skip(pol + 1).ToArray();
+                return Bisekcija(drugepol, podatek);
+            }
+            else if (podatek.CompareTo(tab[pol]) < 0)
+            {
+                T[] prvepol = tab.Take(pol).ToArray();
+                return Bisekcija(prvepol, podatek);
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int[] tab1 = new int[] { 2, 7, 9, 16, 54, 63, 72, 91};
+            string[] tab2 = new string[] {"ana", "anže", "maja", "miha", "mojca", "peter", "petra", "žana", "žan"};
+            Console.WriteLine(Bisekcija(tab1, 17));
+            Console.WriteLine(Bisekcija(tab2, "maja"));
+        }
+    }
+}
---
+++ vaje_16_3/MinMax/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MinMax
+{
+    class Program
+    {
+        public static void IzpisTabele<Karkoli>(Karkoli[] tab)
+        {
+            foreach (Karkoli el in tab)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+        }
+        public static T[] MinMax<T>(T[] tab) where T : IComparable<T>
+        {
+            T max = tab[0];
+            T min = tab[0];
+            foreach (T el in tab)
+            {
+                if (el.CompareTo(max) > 0)
+                {
+                    max = el;
+                }
+                if (el.CompareTo(min) < 0)
+                {
+                    min = el;
+                }
+            }
+            T[] novatab = new T[] {min, max};
+            return novatab;
+        }
+
+        public static string[] MinMax(string[] tab)
+        {
+            string maxEl = tab[0];
+            string minEl = tab[0];
+            foreach (string el in tab)
+            {
+                if (el.Length > maxEl.Length)
+                {
+                    maxEl = el;
+                }
+                if (el.Length < minEl.Length)
+                {
+                    minEl = el;
+                }
+            }
+            string[] novatab = new string[] {minEl, maxEl};
+            return novatab;
+        }
+        static void Main(string[] args)
+        {
+            int[] tab1 = { 12, 5, 6, 78, 3, 5, 6, 21, 434, 56, 42, 7 };
+            string[] tab2 = {"ana", "anže", "maja", "miha", "mateja", "peter", "petra"};
+            IzpisTabele(MinMax(tab1));
+            IzpisTabele(MinMax(tab2));
+        }
+    }
+}
