9a37d11203bf3a43c8561e157a9d56883110f890
Ema Jemec <ema.jemec@student.fmf.uni-lj.si>
2020-03-28 20:42:01 +0100

    vaje 5

---
+++ vaje_23_3/CSV_datoteke/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CSV_datoteke
+{
+    class Program
+    {
+        public static void Datoteke(string vhodna, string izhodna)
+        {
+            if (!File.Exists(vhodna)) throw new Exception("Datoteka ne obstaja!");
+            StreamWriter pisanje = File.CreateText(izhodna);
+            StreamReader branje = File.OpenText(vhodna);
+            string vrstica = branje.ReadLine();
+            string[] tab = vrstica.Split(", ");
+            string ime = tab[0];
+            string starost = tab[1];
+            string teza = tab[2];
+            while (vrstica != null)
+            {
+                vrstica = branje.ReadLine();
+                string[] podatki = vrstica.Split(", ");
+                var parIme = (ime, podatki[0]);
+                var parStarost = (starost, podatki[1]);
+                var parTeza = (teza, podatki[2]);
+                (string, string)[] tabela = {parIme, parStarost, parTeza };
+                pisanje.WriteLine(tabela);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ vaje_23_3/DatBrezOklepajev/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace DatBrezOklepajev
+{
+    class Program
+    {
+        public static void BrezOklepajev(string imeVhod, string imeIzhod)
+        {
+            if (!File.Exists(imeVhod)) throw new Exception("Datoteka ne obstaja!");
+            StreamWriter pisanje = File.CreateText(imeIzhod);
+            StreamReader branje = File.OpenText(imeVhod);
+            string vrstica = branje.ReadLine();
+            while (vrstica != null)
+            {
+                foreach (char znak in vrstica)
+                {
+                    if(znak.Equals("("))
+                    {
+                        //izbriši vse do znaka ")"
+                    }
+                }
+                vrstica = branje.ReadLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
